cc382d99dc05661088e201814e725fcb54ae5a17
wxz97121 <wxz97121@gmail.com>
2020-04-18 15:21:04 +0800

    basic control

---
+++ LD_46/Assets/Character.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    Rigidbody2D m_Rigid;
+    public float MoveForce = 3;
+    public float MouseForce = 30;
+    private void Awake()
+    {
+        m_Rigid = GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        float y = Input.GetAxis("Vertical");
+        float x = Input.GetAxis("Horizontal");
+        m_Rigid.AddForce(MoveForce * new Vector2(x, y));
+        if (Input.GetMouseButtonDown(1))
+        {
+            print("Fuck");
+            Vector2 MousePos = Input.mousePosition;
+            MousePos = Camera.main.ScreenToWorldPoint(MousePos);
+            Debug.DrawLine(m_Rigid.transform.position, MousePos);
+            m_Rigid.AddForce((MousePos - m_Rigid.position).normalized * MouseForce);
+        }
+    }
+}
