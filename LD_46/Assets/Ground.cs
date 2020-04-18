a698ac9baf279ba3de891de97eb3f67560f374db
wxz97121 <wxz97121@gmail.com>
2020-04-18 22:30:10 +0800

    瞎写

---
+++ LD_46/Assets/Ground.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour
+{
+    public float LinearDrag = 0.05f;
+    public float AngularDrag = 0.1f;
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") || collision.CompareTag("it"))
+        {
+            collision.GetComponent<Rigidbody2D>().drag = LinearDrag;
+            collision.GetComponent<Rigidbody2D>().angularDrag = AngularDrag;
+        }
+    }
+}
---
+++ LD_46/Assets/Money.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Money : MonoBehaviour
+{
+    public float Force = 3;
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("it"))
+        {
+            var Dir = (transform.position - collision.transform.position);
+            Dir.z = 0;
+            if (Dir.magnitude < 0.02f) return;
+            Dir.Normalize();
+            collision.GetComponent<Rigidbody2D>().AddForce(new Vector2(Dir.x, Dir.y) * Force);
+        }
+
+    }
+}
