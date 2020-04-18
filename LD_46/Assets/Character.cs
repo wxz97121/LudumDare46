052f56b7aa4a1fb5a3131a2f685cd86cb74412d2
wxz97121 <wxz97121@gmail.com>
2020-04-18 15:39:20 +0800

    旋转

--- LD_46/Assets/Character.cs
+++ LD_46/Assets/Character.cs
@@ -6,7 +6,8 @@ public class Character : MonoBehaviour
 {
     Rigidbody2D m_Rigid;
     public float MoveForce = 3;
-    public float MouseForce = 30;
+    public float MouseForce = 100;
+    public float RotateForce = 3;
     private void Awake()
     {
         m_Rigid = GetComponent<Rigidbody2D>();
@@ -16,13 +17,30 @@ public class Character : MonoBehaviour
         float y = Input.GetAxis("Vertical");
         float x = Input.GetAxis("Horizontal");
         m_Rigid.AddForce(MoveForce * new Vector2(x, y));
-        if (Input.GetMouseButtonDown(1))
+        if (Input.GetMouseButtonDown(0))
         {
-            print("Fuck");
+            //print("Fuck");
             Vector2 MousePos = Input.mousePosition;
             MousePos = Camera.main.ScreenToWorldPoint(MousePos);
-            Debug.DrawLine(m_Rigid.transform.position, MousePos);
-            m_Rigid.AddForce((MousePos - m_Rigid.position).normalized * MouseForce);
+            //Debug.DrawLine(m_Rigid.transform.position, MousePos);
+            m_Rigid.AddForce((MousePos - m_Rigid.position).normalized * MouseForce, ForceMode2D.Impulse);
         }
+        if (Input.GetKey(KeyCode.I))
+        {
+            m_Rigid.AddForce(transform.right * MoveForce, 0);
+        }
+        if (Input.GetKey(KeyCode.K))
+        {
+            m_Rigid.AddForce(transform.right * MoveForce * -1, 0);
+        }
+        if (Input.GetKey(KeyCode.J))
+        {
+            m_Rigid.AddTorque(RotateForce);
+        }
+        if (Input.GetKey(KeyCode.K))
+        {
+            m_Rigid.AddTorque(-1 * RotateForce);
+        }
+
     }
 }
